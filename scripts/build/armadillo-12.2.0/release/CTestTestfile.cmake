31d9cc6d0f7ff8cf58af1c0d1c093d055b80d2ce
Subrat <subrat.net@gmail.com>
2023-05-16 15:24:03 +0530

    Scripts to build Armadillo & other thirdparty - IN PROGRESS

--- scripts/build/armadillo-12.2.0/release/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/subrat/sandbox/URT-N/urt_deps/source/armadillo-12.2.0
-# Build directory: /home/subrat/sandbox/URT-N/urt_deps/scripts/build/armadillo-12.2.0/release
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("tests1")
